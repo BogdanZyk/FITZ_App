a9ec233389d22f62c23d2ffa5af5bfdb5289dd48
Bogdan Zykov <b.zykov@brightlab.me>
2022-09-13 23:58:56 +0300

    Initial Commit

---
+++ FITZ/FITZApp.swift
@@ -0,0 +1,17 @@
+//
+//  FITZApp.swift
+//  FITZ
+//
+//  Created by Bogdan Zykov on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct FITZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
